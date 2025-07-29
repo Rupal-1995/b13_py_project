9f287437d32c664a43a91c3303fb5e5ca9d25bdf
Rupal Shrivastava <rupal@example.com>
2025-07-29 22:29:33 +0530

    add new function

--- abc.py
+++ abc.py
@@ -3,3 +3,10 @@ def hello():
     for i in range(5):
         print(i)
      
+def greet(name):
+    
+    print(f"Hello, {name}!")
+    return f"Greetings, {name}!"
+
+def add(a, b):
+    return a + b
